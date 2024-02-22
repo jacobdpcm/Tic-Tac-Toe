cbcac531acf449b2b16439e66e798e6013abd73c
JacobPCM <jacobd@postcardmania.com>
2024-02-22 17:33:18 -0500

    Set basic skeleton or site

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 //IIFEs
 const gameboard = (function(){
-    let cells = [0,0,0,2,2,0,0,0,0];
+    let cells = [0,0,0,0,0,0,0,0,0];
 
     const resetBoard = cells => {cells = [0,0,0,0,0,0,0,0,0]};
-    const changeCell = (index, player) => {if(cells[index] === 0){cells[index] = player} else return 'error'};
-    const getBoardState = () => {return cells};
+    const changeCell = (index, player) => {
+        if(cells[index] === 0){cells[index] = player} else {return 'error'};
+        winCheck(player);
+        getBoardState();
+    };
+    const getBoardState = () => {return console.log(cells)};
     const winCheck = (player) => {
-        if( cells[0] === cells[1] === cells[2] === player || 
-            cells[3] === cells[4] === cells[5] === player || 
-            cells[6] === cells[7] === cells[8] === player || 
-            cells[0] === cells[3] === cells[6] === player || 
-            cells[1] === cells[4] === cells[7] === player || 
-            cells[2] === cells[5] === cells[8] === player || 
-            cells[0] === cells[4] === cells[8] === player || 
-            cells[2] === cells[4] === cells[6] === player ){
+        if( (cells[0] === cells[1] && cells[1] === cells[2] && cells[2] === player) || 
+            (cells[3] === cells[4] && cells[4] === cells[5] && cells[5] === player) || 
+            (cells[6] === cells[7] && cells[7] === cells[8] && cells[8] === player) || 
+            (cells[0] === cells[3] && cells[3] === cells[6] && cells[6] === player) || 
+            (cells[1] === cells[4] && cells[4] === cells[7] && cells[7] === player) || 
+            (cells[2] === cells[5] && cells[5] === cells[8] && cells[8] === player) || 
+            (cells[0] === cells[4] && cells[4] === cells[8] && cells[8] === player) || 
+            (cells[2] === cells[4] && cells[4] === cells[6] && cells[6] === player) ){
                 console.log('player'+ player + ' winner')
             } else if(!cells.includes(0)) {console.log('tie game')}
     }
 
     return {resetBoard, changeCell, getBoardState, winCheck,}
 })();
+
+const domControl = function(){
+    
+}
 
 //Normal Factories
 const newPlayer = function(name){
@@ -34,6 +42,7 @@ const newPlayer = function(name){
 
 const one = newPlayer('one');
 const two = newPlayer('two');
+one.makeTurn();
 
 
 
