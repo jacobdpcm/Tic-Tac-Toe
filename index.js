19ced62a247bd0b0db271a6e094d642b4d9dc9cb
JacobPCM <jacobd@postcardmania.com>
2024-02-23 17:31:06 -0500

    Partial UI Missing Win and New Round

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //IIFEs
 const gameboard = (function(){
     let cells = [0,0,0,0,0,0,0,0,0];
+    let turn = 1;
 
     const resetBoard = cells => {cells = [0,0,0,0,0,0,0,0,0]};
-    const changeCell = (index, player) => {
-        if(cells[index] === 0){cells[index] = player} else {return 'error'};
-        winCheck(player);
+    const changeCell = (index, playerTurn) => {
+        if(cells[index] === 0){cells[index] = playerTurn} else {return 'error'};
+        winCheck(playerTurn);
         getBoardState();
     };
     const getBoardState = () => {return console.log(cells)};
@@ -18,31 +19,85 @@ const gameboard = (function(){
             (cells[2] === cells[5] && cells[5] === cells[8] && cells[8] === player) || 
             (cells[0] === cells[4] && cells[4] === cells[8] && cells[8] === player) || 
             (cells[2] === cells[4] && cells[4] === cells[6] && cells[6] === player) ){
-                console.log('player'+ player + ' winner')
-            } else if(!cells.includes(0)) {console.log('tie game')}
+                return player
+            } else if(!cells.includes(0)) {
+                return 3
+            } else {return 0}     
     }
-
-    return {resetBoard, changeCell, getBoardState, winCheck,}
-})();
-
-const domControl = function(){
     
-}
-
-//Normal Factories
-const newPlayer = function(name){
-    let turn = 0;
-
-    const makeTurn = () => turn = 1;
+    const makeTurn = () => {
+        if(turn === 0 || turn === 2){
+            turn = 1
+        } else {turn = 2}
+    };
     const endTurn = () => turn = 0;
     const getTurn = () => {return turn};
 
-    return {name, makeTurn, endTurn, getTurn,}
+    return {resetBoard, changeCell, getBoardState, winCheck, makeTurn, endTurn, getTurn,}
+})();
+
+const domControl = (function(){
+    const player1 = document.querySelector('.one');
+    const player2 = document.querySelector('.two');
+    const cells = document.querySelectorAll('.cell');
+
+    const changeTurn = () => {
+        player1.classList.remove('turn1');
+        player2.classList.remove('turn2');
+        let turn = gameboard.getTurn()
+        if(turn === 0){
+            return
+        } else if(turn === 1){
+            player1.classList.add('turn1');
+        }
+        else if(turn === 2) {
+            player2.classList.add('turn2');
+        }  
+    }
+
+    const fullTurn = (nodeIndex, playerTurn) => {
+        gameboard.changeCell(nodeIndex, playerTurn);
+        const winCheck = gameboard.winCheck(playerTurn);
+        if(winCheck === 1){
+            //LEFT OFF HERE FIGURE OUT HOW TO REMOVE EVENT LISTENERS
+        }
+
+        gameboard.makeTurn();
+        changeTurn();
+    }
+    
+    const updateBoard = () => {
+        cells.forEach((cell, index) => {cell.addEventListener('click', () => {
+            let turn = gameboard.getTurn();
+            if(turn === 1 && cell.textContent === ''){
+                cell.textContent = 'X';
+                fullTurn(index, turn);
+            } else if(turn === 2 && cell.textContent=== ''){
+                cell.textContent = 'O';
+                fullTurn(index, turn);
+            }
+            
+            
+            
+        })})
+    }
+
+    return {changeTurn, updateBoard,}
+})();
+
+//Normal Factories
+const newPlayer = function(name){
+    let score = 0;
+
+    const updateScore = () => score++
+
+    return {name, updateScore,}
 }
 
 const one = newPlayer('one');
 const two = newPlayer('two');
-one.makeTurn();
+
+domControl.updateBoard()
 
 
 
